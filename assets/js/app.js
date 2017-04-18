87c438f05efd16721c6dd5b558e7f837214d3a2c
pilarfc <pilar.fc@outlook.com>
2017-04-18 15:46:55 +0000

    ya funcionan los botones origen e historia

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,37 @@
+var botonOrigen =  document.getElementById("botonOrigen"); 
+var botonHistoria = document.getElementById("botonHistoria");
+var botonRestaurar = document.getElementById("botonRestaurar"); 
+
+var textoOrigen = document.getElementById("textoOrigen");
+var textoHistoria = document.getElementById("textoHistoria"); 
+
+
+botonOrigen.addEventListener("click", mostrartextoOrigen);
+botonOrigen.addEventListener("dblclick", ocultartextoOrigen); 
+botonHistoria.addEventListener("click", mostrartextoHistoria);
+botonHistoria.addEventListener("dblclick", ocultartextoHistoria);
+
+
+
+function mostrartextoOrigen () {
+    textoOrigen.style.visibility = "visible";
+}
+
+function ocultartextoOrigen () {
+ textoOrigen.style.visibility = "hidden"; 
+}
+
+function mostrartextoHistoria () {
+    textoHistoria.style.visibility = "visible";
+}
+
+function ocultartextoHistoria () {
+    textoHistoria.style.visibility = "hidden"; 
+}
+
+
+
 function borrarImagen () {
 document.getElementsByClassName("pandas").style.visibility = "hidden";
 }
+
